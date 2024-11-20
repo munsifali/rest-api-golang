3f6361325722752cf1337a562c7b685b904925e1
munsifali <munsifali_ali@hotmail.com>
2024-11-20 11:47:24 +0500

    make server with http request

--- cmd/rest-api-golang/main.go
+++ cmd/rest-api-golang/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/munsifali/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("yes")
+	/*
+		Load Configuration
+		Setup Database
+		Setup Router
+		Setup Http Server
+	*/
+	configuration := config.MustLoad()
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to student Api"))
+	})
+
+	server := http.Server{
+		Addr:    configuration.Address,
+		Handler: router,
+	}
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Faild to start server!")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting Down Server!")
+
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,10 @@ package config
 
 import (
 	"flag"
+	"log"
 	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
@@ -17,12 +20,29 @@ type Config struct {
 	HTTPServer  `yaml:"http_server"`
 }
 
-func MustLoad() {
+func MustLoad() *Config {
 	var configPath string
 	configPath = os.Getenv("CONFIG_PATH")
+
 	if configPath == "" {
 		flags := flag.String("config", "", "path to the configuration file")
 		flag.Parse()
 		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Configuration not Found!")
+		}
 	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("Cannot Read Config File %s", err.Error())
+	}
+	return &cfg
 }
